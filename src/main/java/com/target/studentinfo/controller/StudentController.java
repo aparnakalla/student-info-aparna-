bc90bb556ad87365114cd45a012232ea49f42d8c
Aparna Kalla <aparnakalla11@gmail.com>
2023-10-10 11:28:27 +0530

    Validations added in StudentRequest

--- src/main/java/com/target/studentinfo/controller/StudentController.java
+++ src/main/java/com/target/studentinfo/controller/StudentController.java
@@ -2,13 +2,23 @@ package com.target.studentinfo.controller;
 import com.target.studentinfo.dto.mapper.StudentMapper;
 import com.target.studentinfo.dto.request.StudentRequest;
 import com.target.studentinfo.dto.response.StudentResponse;
+import com.target.studentinfo.model.AllValidationErrorsGroup;
 import com.target.studentinfo.model.Student;
 import com.target.studentinfo.service.StudentService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+
+//import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
+@Validated
 public class StudentController {
     private final StudentService studentService;
     private final StudentMapper studentMapper;
@@ -37,11 +47,43 @@ public class StudentController {
     }
 
     @PostMapping("/students")
-    public StudentResponse addStudent(@RequestBody StudentRequest studentRequest){
+    public ResponseEntity<Object> addStudent(@Validated(AllValidationErrorsGroup.class) @RequestBody StudentRequest studentRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errorMessages = bindingResult.getAllErrors()
+                    .stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+
+            return ResponseEntity.badRequest().body(errorMessages);
+        }
+
+        // If there are no validation errors, proceed to add the student
         Student student = studentMapper.toStudent(studentRequest);
         Student addedStudent = studentService.addStudent(student);
-        return studentMapper.toStudentResponse(addedStudent);
+        return ResponseEntity.ok(studentMapper.toStudentResponse(addedStudent));
     }
+//    public ResponseEntity<?> addStudent(@Valid @RequestBody StudentRequest studentRequest, BindingResult bindingResult) {
+//        if (bindingResult.hasErrors()) {
+//
+//            // Handle validation errors and return an appropriate response
+//            List<String> errorMessages = bindingResult.getAllErrors()
+//                    .stream()
+//                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+//                    .collect(Collectors.toList());
+//
+//            return ResponseEntity.badRequest().body(errorMessages);
+//        }
+//
+//        // If there are no validation errors, proceed to add the student
+//        Student student = studentMapper.toStudent(studentRequest);
+//        Student addedStudent = studentService.addStudent(student);
+//        return ResponseEntity.ok(studentMapper.toStudentResponse(addedStudent));
+//    }
+//    public StudentResponse addStudent(@RequestBody StudentRequest studentRequest){
+//        Student student = studentMapper.toStudent(studentRequest);
+//        Student addedStudent = studentService.addStudent(student);
+//        return studentMapper.toStudentResponse(addedStudent);
+//    }
 
     @PutMapping("/students/{id}")
     public StudentResponse updateStudent(@RequestBody StudentRequest studentRequest, @PathVariable("id") long id) {
@@ -54,4 +96,4 @@ public class StudentController {
     public void deleteStudent(@PathVariable("id") long id) {
         studentService.deleteStudent(id);
     }
-   }
+}
--- src/main/java/com/target/studentinfo/model/StudentValidator.java
+++ src/main/java/com/target/studentinfo/model/StudentValidator.java
@@ -1,26 +1,26 @@
-package com.target.studentinfo.model;
-
-import com.target.studentinfo.exception.BadRequestException;
-import com.target.studentinfo.exception.ErrorCode;
-import com.target.studentinfo.exception.ErrorMessage;
-import com.target.studentinfo.model.Student;
-
-public class StudentValidator {
-    public static void validateStudent(Student student) {
-        if (student == null) {
-            throw new BadRequestException(ErrorCode.INVALID_REQUEST_BODY, ErrorMessage.INVALID_REQUEST_BODY);
-        }
-
-        if (student.getFirstName() == null) {
-            throw new BadRequestException(ErrorCode.INVALID_FIRST_NAME, ErrorMessage.INVALID_FIRST_NAME);
-        }
-
-        if (student.getLastName() == null) {
-            throw new BadRequestException(ErrorCode.INVALID_LAST_NAME, ErrorMessage.INVALID_LAST_NAME);
-        }
-
-        if (student.getStandard() < 1 || student.getStandard() > 10) {
-            throw new BadRequestException(ErrorCode.INVALID_STANDARD, ErrorMessage.INVALID_STANDARD);
-        }
-    }
-}
+//package com.target.studentinfo.model;
+//
+//import com.target.studentinfo.exception.BadRequestException;
+//import com.target.studentinfo.exception.ErrorCode;
+//import com.target.studentinfo.exception.ErrorMessage;
+//import com.target.studentinfo.model.Student;
+//
+//public class StudentValidator {
+//    public static void validateStudent(Student student) {
+//        if (student == null) {
+//            throw new BadRequestException(ErrorCode.INVALID_REQUEST_BODY, ErrorMessage.INVALID_REQUEST_BODY);
+//        }
+//
+//        if (student.getFirstName() == null) {
+//            throw new BadRequestException(ErrorCode.INVALID_FIRST_NAME, ErrorMessage.INVALID_FIRST_NAME);
+//        }
+//
+//        if (student.getLastName() == null) {
+//            throw new BadRequestException(ErrorCode.INVALID_LAST_NAME, ErrorMessage.INVALID_LAST_NAME);
+//        }
+//
+//        if (student.getStandard() < 1 || student.getStandard() > 10) {
+//            throw new BadRequestException(ErrorCode.INVALID_STANDARD, ErrorMessage.INVALID_STANDARD);
+//        }
+//    }
+//}
